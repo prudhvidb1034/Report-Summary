08d39d4bda1a318fcbdd820cd14ab8412290482f
prudhvidb1034 <prudhvidb1034@gmail.com>
2025-05-12 11:23:20 +0530

    intial Chnags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { IonButton } from '@ionic/angular/standalone';
+import { IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
+import { IonInput, IonItem, IonList } from '@ionic/angular/standalone';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,IonButton,IonFab, IonInput, IonItem, IonList,IonFabButton, IonIcon,IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

1c142578d7c80496cf03d3ad6e67dbb0967a5f68
Sarath Chandra <chandutenneti30@gmail.com>
2025-06-23 09:15:35 +0530

    #changes on login page

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -6,6 +6,11 @@ import { CommonModule } from '@angular/common';
 import { ToastService } from '../../shared/toast.service';
 import { RouterLink } from '@angular/router';
 
+interface LoginCredentials {
+  username: string;
+  password: string;
+}
+
 
 @Component({
   selector: 'app-login',
@@ -15,29 +20,57 @@ import { RouterLink } from '@angular/router';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  loginForm: FormGroup;
-  loginStore = inject(LoginStore);
-  private toast = inject(ToastService)
-  constructor(private fb: FormBuilder) {
-    this.loginForm = this.fb.group({
-      username: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
-    });
-    // this.loginForm.controls['username'].setValue(localStorage.getItem('username'));
-    // this.loginForm.controls['password'].setValue(localStorage.getItem('password'))
+   constructor(private fb: FormBuilder) {}
+      loginStore = inject(LoginStore);
 
+  // loginForm: FormGroup;
+  // loginStore = inject(LoginStore);
+  // private toast = inject(ToastService)
+  // constructor(private fb: FormBuilder) {
+  //   this.loginForm = this.fb.group({
+  //     username: ['', [Validators.required, Validators.email]],
+  //     password: ['', [Validators.required, Validators.minLength(6)]]
+  //   });
+  //   // this.loginForm.controls['username'].setValue(localStorage.getItem('username'));
+  //   // this.loginForm.controls['password'].setValue(localStorage.getItem('password'))
+
+  // }
+
+
+  // onSubmit() {
+  //   // localStorage.setItem('username',this.loginForm.value.username);
+  //   // localStorage.setItem('password',this.loginForm.value.password);
+  //   console.log(this.loginForm.value)
+  //   if (this.loginForm.valid) {
+  //     this.loginStore.login(this.loginForm.value);
+  //   } else {
+  //     this.toast.show('error', 'Please fill in all required fields.')
+  //   }
+  // }
+showPwd = false;
+form = this.fb.group({
+  username: ['', [Validators.required, Validators.email]],
+  password: ['', Validators.required]
+});
+
+togglePw() {
+  this.showPwd = !this.showPwd;
+}
+submit() {
+  if (this.form.valid) {
+    console.log(this.form.value);
+    const fv=this.form.value
+      const creds: LoginCredentials = {
+    username: fv.username?fv.username:'',
+    password: fv.password?fv.password:''
+  };
+    this.loginStore.login(creds);
+    // your API call here
   }
-
-
-  onSubmit() {
-    // localStorage.setItem('username',this.loginForm.value.username);
-    // localStorage.setItem('password',this.loginForm.value.password);
-    console.log(this.loginForm.value)
-    if (this.loginForm.valid) {
-      this.loginStore.login(this.loginForm.value);
-    } else {
-      this.toast.show('error', 'Please fill in all required fields.')
-    }
-  }
-
+}
+googleSignIn() {
+  // call Capacitor GoogleAuth or Firebase login
+}
+forgot() { /* nav to forgot */ }
+signUp() { /* nav to sign-up */ }
 }

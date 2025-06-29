94987666fdfd95b0e135d4079e2bad6a619d8292
User <user@Users-MacBook-Air.local>
2025-06-29 13:15:39 +0530

    ai changes

--- src/app/features/ai-chatbot/ai-chatbot.component.ts
+++ src/app/features/ai-chatbot/ai-chatbot.component.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { GemeniAiService } from '../../services/gemeni-ai/gemeni-ai.service';
 import { GemeniUpdatedAIService } from '../../services/gemeni-ai/gemeni-updated-ai-service';
+import { GENAI } from '../../services/gemeni-ai/gemeni-geni-service';
 
 @Component({
   selector: 'app-ai-chatbot',
@@ -19,33 +20,33 @@ export class AiChatbotComponent {
   messages: { text: string; type: 'user' | 'ai' }[] = [];
   res:any;
 
-  constructor(private http:HttpClient,private aiService:GemeniAiService,private updatedGemeniAI:GemeniUpdatedAIService){}
+  constructor(private http:HttpClient,private aiService:GemeniAiService,private updatedGemeniAI:GemeniUpdatedAIService,private genAi:GENAI){}
 
   submitQuestion(): void {
-    this.updatedGemeniAI.main();
-    if (this.userInput.trim()) {
-      // Append user's question
-      this.messages.push({ text: `You: ${this.userInput}`, type: 'user' });
-       this.res=this.aiService.getAIExplanation(this.userInput);
-      if(this.res){
-        this.messages.push({ text: this.res, type: 'ai' });
-      }
-      const params=this.userInput
-      // this.http.get('/api/gemini'+'/'+this.userInput).subscribe((data:any)=>{
-      //   if(data){
-      //     this.messages.push({ text: data.response, type: 'ai' });
-      //   }
+    this.genAi.main();
+    // if (this.userInput.trim()) {
+    //   // Append user's question
+    //   this.messages.push({ text: `You: ${this.userInput}`, type: 'user' });
+    //    this.res=this.aiService.getAIExplanation(this.userInput);
+    //   if(this.res){
+    //     this.messages.push({ text: this.res, type: 'ai' });
+    //   }
+    //   const params=this.userInput
+    //   // this.http.get('/api/gemini'+'/'+this.userInput).subscribe((data:any)=>{
+    //   //   if(data){
+    //   //     this.messages.push({ text: data.response, type: 'ai' });
+    //   //   }
 
-      //   });
-      console.log(this.userInput);
-     // this.http.post('http://localhost:3000/gemeni','')
-      // Simulate AI response (you would replace this with actual AI logic)
-      const aiResponse = "AI: This is a simulated response to your question.";
-    //  this.messages.push({ text: aiResponse, type: 'ai' });
+    //   //   });
+    //   console.log(this.userInput);
+    //  // this.http.post('http://localhost:3000/gemeni','')
+    //   // Simulate AI response (you would replace this with actual AI logic)
+    //   const aiResponse = "AI: This is a simulated response to your question.";
+    // //  this.messages.push({ text: aiResponse, type: 'ai' });
 
-      // Clear the input field
-      this.userInput = '';
-    }
+    //   // Clear the input field
+    //   this.userInput = '';
+    // }
    // this.messages.push({ text: 'Iam an AI ', type: 'ai' });
   }
 

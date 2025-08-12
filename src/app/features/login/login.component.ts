df4d889a35895c13d88b8dfc5f164dcbf7b41549
Sarath Chandra <chandutenneti30@gmail.com>
2025-08-12 12:55:41 +0530

    #lint changes

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,10 +1,8 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { LoginStore } from '../../state/login.store';
 import { CommonModule } from '@angular/common';
-import { ToastService } from '../../shared/toast.service';
-import { RouterLink } from '@angular/router';
 
 interface LoginCredentials {
   userName: string;

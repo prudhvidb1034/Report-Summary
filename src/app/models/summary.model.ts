d0f229bbff06b9878cffa119020bf937157fa6a9
Phaneendra <phani.gatta@gmail.com>
2025-09-03 13:07:37 +0530

    account test casses and summmary component lint issues

--- src/app/models/summary.model.ts
+++ src/app/models/summary.model.ts
@@ -1,24 +1,24 @@
 export interface DailyUpdate {
-    date: string;       
-    status: string;     
-    comments: string;
-  }
-  
-  export interface Employee {
-    employee_id: string;
-    employee_name: string;
-    daily_updates: DailyUpdate[];
-  }
-  
-  export interface Project {
-    project_id: string;
-    project_name: string;
-    start_date: string; 
-    end_date: string; 
-    employees: Employee[];
-  }
+  date: string;
+  status: string;
+  comments: string;
+}
 
- export interface WeeklyDataResponse {
+export interface Employee {
+  employee_id: string;
+  employee_name: string;
+  daily_updates: DailyUpdate[];
+}
+
+export interface Project {
+  project_id: string;
+  project_name: string;
+  start_date: string;
+  end_date: string;
+  employees: Employee[];
+}
+
+export interface WeeklyDataResponse {
   data: WeeklyData;
 }
 
@@ -29,12 +29,42 @@ export interface WeeklyData {
   content: WeeklyEntry[];
 }
 
+// export interface WeeklyEntry {
+//   weekId: number;
+//   weekNo: number
+//   weekStartDate: string; // ISO format date, e.g., "2025-07-14"
+//   weekEndDate: string;   // ISO format date
+//   upcomingTasks: string[];
+//   projectNames: string[];
+//   weekRange: string; 
+//   status: string;    // Formatted date range string
+// }
+
+// export interface WeeklyEntry {
+//   weekId: string;
+//   weekNo: number;
+//   status: string;
+//   viewTask: string;
+//   viewReport: string;
+//   weekStartDate?: string;
+//   weekEndDate?: string;
+//   upcomingTasks?: string[];
+//   projectNames?: string[];
+//   weekRange?: string;
+// }
+
 export interface WeeklyEntry {
-  weekId: number;
-  weekStartDate: string; // ISO format date, e.g., "2025-07-14"
-  weekEndDate: string;   // ISO format date
-  upcomingTasks: string[];
-  projectNames: string[];
-  weekRange: string;     // Formatted date range string
+  weekId: string;
+  weekNo: number;
+  status: string;
+  viewTask: string;
+  viewReport: string;
+  weekStartDate?: string;
+  weekEndDate?: string;
+  upcomingTasks?: string[];
+  projectNames?: string[];
+  weekRange: { weekFromDate: string; weekToDate: string };
 }
+
+
 
